1c3b815a019559733d386fb7585109e88a3c52cb
Alexandr Krivosheya <a.krivosheya@g.nsu.ru>
2023-03-06 12:12:23 +0700

    Refactor Items

--- Assets/Scripts/Items/DialogPoint.cs
+++ Assets/Scripts/Items/DialogPoint.cs
@@ -1,20 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ObjectData))]
+
 public class DialogPoint : MonoBehaviour, Interactable
 {
-    public int Id { get; private set; } = 5;//лучше где-то взять
     [SerializeField] private List<string> AllConditions;
     [SerializeField] private List<string> Dialogs;
     [SerializeField] private List<string> Persons;
     [SerializeField] private List<string> NewConditions;
     [SerializeField] private List<string> DeletingConditions;
     [SerializeField] private List<string> Messages;
+    [SerializeField] private char SplitCharacter;
+    private ObjectData _objectData;
 
     void Start()
     {
-        Managers.Scene.SetObjectPosition(Id, transform.position);
+        _objectData.Id = ObjectsId.DialogPoint;
+        Managers.Scene.SetObjectPosition((int)_objectData.Id, transform.position);
     }
 
     public void Interact(int interactingId)
@@ -22,7 +25,7 @@ public class DialogPoint : MonoBehaviour, Interactable
         for(int i = 0; i < AllConditions.Count; ++i)
         {
             var conditionsString = AllConditions[i];
-            var conditionsArray = conditionsString.Split(';');//константа
+            var conditionsArray = conditionsString.Split(SplitCharacter);
             bool correctDialog = true;
             foreach(string condition in conditionsArray)
             {
@@ -36,19 +39,19 @@ public class DialogPoint : MonoBehaviour, Interactable
             {
                 continue;
             }
-            foreach(var condition in NewConditions[i].Split(';'))//константа
+            foreach(var condition in NewConditions[i].Split(SplitCharacter))
             {
                 Managers.Conditions.AddCondition(condition);
             }
-            foreach(var condition in DeletingConditions[i].Split(';'))//константа
+            foreach(var condition in DeletingConditions[i].Split(SplitCharacter))
             {
                 Managers.Conditions.DeleteCondition(condition);
             }
-            foreach(var message in Messages[i].Split(';'))//константа
+            foreach(var message in Messages[i].Split(SplitCharacter))
             {
                 Messenger.Broadcast(message);
             }
-            Managers.Dialogs.StartDialog(Dialogs[i].Split(';'), Persons[i].Split(';'));//константа
+            Managers.Dialogs.StartDialog(Dialogs[i].Split(SplitCharacter), Persons[i].Split(SplitCharacter));
             break;
         }
     }
